d2e7b6bd9f562ad1391738c9d1c2ef7c66265d58
rampantspark <80992075+rampantspark@users.noreply.github.com>
2022-07-27 11:34:18 -0500

    commit from linux dev machine

--- backup.py
+++ backup.py
@@ -3,23 +3,25 @@ from tkinter.ttk import *
 import tkinter.messagebox as msg
 import json
 
+# Global vars
+tree = None
+location_count = 0
+btnEditSchedule = None
+btnEditScheduleEdit = None
+cmbSelected = None
+cmbSelectedEdit = None
+cmbSchedule = None
+
 class MainApplication(Tk):
 
-    tree = None
-    location_count = 0
-    btnEditSchedule = None
-    btnEditScheduleEdit = None
-    cmbSelected = None
-    cmbSelectedEdit = None
-      
     def __init__(self):
         super().__init__()
-        
+
         self.title("backup.py")
         self.geometry("750x500")
         self.resizable(False, False)
         #self.iconphoto(False, PhotoImage(file="assets/title_icon.png"))
-        
+
         global tree
         # MAIN WINDOW GUI
         columns = ('id', 'name', 'source', 'destination', 'schedule', 'last_backup', 'next_backup', 'status')
@@ -53,21 +55,23 @@ class MainApplication(Tk):
         btnAdd = Button(self, text="Add Source...", command=self.openAddSourceWindow)
         btnEdit = Button(self, text="Edit Source...", command=self.openEditSourceWindow)
         btnDelete = Button(self, text="Remove Source...", command=self.removeSource)
-        btnBackup = Button(self, text="Backup Now...")
+        btnBackup = Button(self, text="Backup...")
+        btnBackupNow = Button(self, text="Backup All")
         # Push Title to Grid
-        lblTitle.grid(column=0, row=0, columnspan=4, padx=0, pady=30)
+        lblTitle.grid(column=0, row=0, columnspan=5, padx=0, pady=30)
         # Push the Frame, Tree View, and Scrollbar to Grid
-        frame.grid(column=0, row=1, columnspan=4, padx=20, pady=20)
-        tree.grid(column=0, row=1, columnspan=4, padx=0, pady=0, sticky='nsew')
-        scrlTree.grid(column=4, row=1, padx=0, sticky="ns")
+        frame.grid(column=0, row=1, columnspan=5, padx=20, pady=20)
+        tree.grid(column=0, row=1, columnspan=5, padx=0, pady=0, sticky='nsew')
+        scrlTree.grid(column=5, row=1, padx=0, sticky="ns")
         # Populate the Tree with JSON location data
         self.populate_tree(self)
         # Push Main Functionality Buttons to Grid
-        btnAdd.grid(column=0, row=4, padx=15, pady=20)
-        btnEdit.grid(column=1, row=4, padx=15, pady=20)
-        btnDelete.grid(column=2, row=4, padx=15, pady=20)
-        btnBackup.grid(column=3, row=4, padx=15, pady=20)
-    
+        btnAdd.grid(column=0, row=4, padx=5, pady=20)
+        btnEdit.grid(column=1, row=4, padx=5, pady=20)
+        btnDelete.grid(column=2, row=4, padx=5, pady=20)
+        btnBackup.grid(column=3, row=4, padx=5, pady=20)
+        btnBackupNow.grid(column=4, row=4, padx=5, pady=20)
+
     def populate_tree(event, self):
         global tree
         global location_count
@@ -79,14 +83,14 @@ class MainApplication(Tk):
 
         for d in data:
             tree.insert('', END, values=(d['id'], d['name'], d['source'], d['destination'], d['schedule'], d['lastbackup'], d['nextbackup'], d['status']))
-            self.location_count +=1
-            print(self.location_count)
-    
-    def clearTree():
+            location_count +=1
+            print(location_count)
+
+    def clearTree(event):
         #clear the tree
         for item in tree.get_children():
                 tree.delete(item)
-    
+
     def openAddSourceWindow(self):
         # Toplevel object which will
         # be treated as a new window
@@ -95,7 +99,8 @@ class MainApplication(Tk):
         # Toplevel widget
         addSourceWindow.title("Add Source...")
         # sets the geometry of toplevel
-        addSourceWindow.geometry("400x300") 
+        addSourceWindow.geometry("400x300")
+        addSourceWindow.resizable(False, False)
         global btnEditSchedule
         global cmbSelected
         lblName = Label(addSourceWindow, text="Name:")
@@ -107,7 +112,7 @@ class MainApplication(Tk):
         lblSchedule = Label(addSourceWindow, text="Schedule:")
         cmbSelected = StringVar()
         cmbSchedule = Combobox(addSourceWindow, textvariable= cmbSelected)
-        
+
         cmbSelected.trace('w', self.switchStateAdd)
         cmbSchedule['values'] = ("Never", "Weekly", "Every Two Weeks")
         btnEditSchedule = Button(addSourceWindow, text="Change Schedule", state=DISABLED, command=self.openEditScheduleWindow)
@@ -126,7 +131,7 @@ class MainApplication(Tk):
         btnEditSchedule.grid(column=1, row=4, padx=50, pady=10)
         btnCancel.grid(column=0, row=5)
         btnSubmit.grid(column=1, row=5)
-    
+
         return addSourceWindow
 
     def switchStateAdd(*arg):
@@ -136,9 +141,10 @@ class MainApplication(Tk):
             btnEditSchedule["state"] = "normal"
 
         print("switchStateAdd was run")
- 
+
     def addSource(self, name, source, destination, schedule, window):
         global location_count
+        print(location_count)
         print(name + source + destination)
         # Make sure the data to save exists
         if not name or not source or not destination:
@@ -175,7 +181,7 @@ class MainApplication(Tk):
             location_count = 0
             # Populate the Tree View
             self.opulateTree()
-    
+
     def openEditScheduleWindow(self):
         editScheduleWindow = Toplevel(self)
 
@@ -190,8 +196,10 @@ class MainApplication(Tk):
         editSourceWindow = Toplevel(self)
         editSourceWindow.title("Edit Source...")
         editSourceWindow.geometry("400x300")
+        editSourceWindow.resizable(False, False)
         global btnEditScheduleEdit
         global cmbSelectedEdit
+        global cmbSchedule
         # Get the currently selected tree view item
         ###### ADD ERROR HANDLING HERE WITH IF STATEMENT
         selected_item = tree.selection()[0]
@@ -214,10 +222,10 @@ class MainApplication(Tk):
         else:
             #throw an exception
             current=3
-            
+
         #cmbSelectedEdit = StringVar()
         #cmbSelectedEditRead = StringVar()
-        
+
         lblName = Label(editSourceWindow, text="Name:")
         txtName = Entry(editSourceWindow)
         txtName.insert(0, name)
@@ -229,7 +237,7 @@ class MainApplication(Tk):
         txtDestination.insert(2, destination)
         lblSchedule = Label(editSourceWindow, text="Schedule:")
         cmbSchedule = Combobox(editSourceWindow)
-        
+
         #cmbSelectedEdit.trace('r', self.switchStateEdit)
         cmbSchedule['values'] = ("Never", "Weekly", "Every Two Weeks")
         cmbSchedule.current(current)
@@ -247,18 +255,20 @@ class MainApplication(Tk):
         lblSchedule.grid(column=0, row=3, padx=50, pady=10)
         cmbSchedule.grid(column=1, row=3, padx=10, pady=10)
         cmbSchedule.bind('<<ComboboxSelected>>', self.switchStateEdit)
-        
+
         btnEditScheduleEdit.grid(column=1, row=4, padx=50, pady=10)
-        
+
         btnCancel.grid(column=0, row=5)
         btnSubmit.grid(column=1, row=5)
-        
+
         #return editSourceWindow
     def readStateEdit(event, self):
+        global cmbSchedule
+        global btnEditScheduleEdit
         print("event")
-        print(event.)
-        if selfcmbSchedule.get() == "Never":
-            btnEditScheduleEdit["state"] = "disabled"
+        print(event)
+        if cmbSchedule.get() == "Never":
+           btnEditScheduleEdit["state"] = "disabled"
         else:
             btnEditScheduleEdit["state"] = "normal"
     def switchStateEdit(event):
@@ -266,8 +276,8 @@ class MainApplication(Tk):
             btnEditScheduleEdit["state"] = "disabled"
         else:
             btnEditScheduleEdit["state"] = "normal"
-            
-        print("switchStateEdit was run")    
+
+        print("switchStateEdit was run")
     # Edit Source Logic
     def editSource(self, id, name, source, destination, schedule, window):
         print(name + source + destination)
@@ -360,7 +370,7 @@ class MainApplication(Tk):
     # Backup All Logic
     def backupAll():
         msg.showwarning("Remove Source...", "Are you sure you want to remove this backup location?")
- 
+
 if __name__ == "__main__":
     app = MainApplication()
     app.mainloop()
